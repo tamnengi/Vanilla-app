404b1b03d47a3bef2a760e9e559591d442699491
tamnengi <104331919+tamnengi@users.noreply.github.com>
2022-06-13 19:31:33 -0500

    js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Hello");
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
+let apiKey = "405fl0bae34e07976044dc55f313e16a";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=405f10bae34e07976244dc55f313e16a&units=metric";
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
