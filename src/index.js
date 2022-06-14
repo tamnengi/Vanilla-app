3e2911654f1da4e8fb4d9e5f80149058d1894b86
tamnengi <104331919+tamnengi@users.noreply.github.com>
2022-06-13 21:57:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
