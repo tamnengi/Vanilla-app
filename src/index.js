44d27d213b46805cb276afb0f683e9d0b05fbbe7
tamnengi <104331919+tamnengi@users.noreply.github.com>
2022-06-13 20:28:51 -0500

    update city and temperature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "405fl0bae34e07976044dc55f313e16a";
