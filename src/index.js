96cc5805422196c7d62dc1687ccd3f543f4d40d5
tamnengi <104331919+tamnengi@users.noreply.github.com>
2022-06-14 17:03:10 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,12 +28,15 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  console.log(response.data);
 }
 
 let apiKey = "405fl0bae34e07976044dc55f313e16a";
