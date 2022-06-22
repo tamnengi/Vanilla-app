58fdbe67f33213c2df318012dda841e1ab044c3b
tamnengi <104331919+tamnengi@users.noreply.github.com>
2022-06-22 16:57:16 -0500

    changed Thur to Thu

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 function displayForecast(response) {
